de87a9704b12ed5d0baa7d6753a033e88b7eadc2
gxz <xizhuogao@foxmail.com>
2024-04-23 20:04:29 +0800

    添加定时清理文件的数据库的代码

---
+++ src/main/java/com/netdisk/mappers/ScheduleMapper.java
@@ -0,0 +1,12 @@
+package com.netdisk.mappers;
+
+public interface ScheduleMapper {
+
+    void updateByRecoveryTime();
+
+    void deleteEmailCode();
+
+    void deleteFile();
+
+    void deleteShare();
+}
---
+++ src/main/java/com/netdisk/service/ScheduleService.java
@@ -0,0 +1,14 @@
+package com.netdisk.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+
+public interface ScheduleService {
+//    @Scheduled(cron = "0 0 4 * * *")//每天凌晨4点执行
+//    void deleteTrash();
+//
+//    @Scheduled(cron = "0 0 0 */7 * ?")  // 每7天执行一次，午夜执行
+//    void deleteDatabase();
+//
+//    @Scheduled(cron = "0 0 3 * * *")  //每天凌晨3点执行
+//    void deleteTempFile();
+}
