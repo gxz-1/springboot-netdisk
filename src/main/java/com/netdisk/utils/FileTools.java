d42b6f6941668b77c0e4fd948906032c48a5ff74
gxz_laptop <xizhuogao@foxmail.com>
2025-02-09 17:52:37 +0800

    多线程下载、限速下载实现逻辑

--- src/main/java/com/netdisk/utils/FileTools.java
+++ src/main/java/com/netdisk/utils/FileTools.java
@@ -16,13 +16,25 @@ public class FileTools {
         if (!file.exists()) {
             return;
         }
-
+        //实现多线程下载
+        //1.将下面的下载代码改为下载某个分片，并包装为一个线程类，继承Runnable
+        //2.通过ExecutorService创建线程池管理每个线程
+        //3.合并下载的分片为目标下载文件
         try (FileInputStream in = new FileInputStream(file);
              //TODO 注意资源一定要及时释放，不然会导致资源被一直占用，在更新头像时报错！
              OutputStream out = response.getOutputStream()) {//try-with-resources语句会自动关闭资源
             byte[] byteData = new byte[1024];
             int len;
             while ((len = in.read(byteData)) != -1) {
+//                //如果需要实现限速下载，1.在这里统计已经下载的字节总数totalBytesRead
+//                long totalBytesRead += len;
+//                //2.根据totalBytesRead计算当前速率并与限制的速率比较
+//                if (totalBytesRead/Time > DOWNLOAD_SPEED_LIMIT) {
+//                    //3.暂停一段时间以控制速率
+//                    long idealTime = (totalBytesRead * 1000) / DOWNLOAD_SPEED_LIMIT;
+//                    long sleepTime = idealTime - Time;
+//                    Thread.sleep(sleepTime);
+//                }
                 out.write(byteData, 0, len);
             }
             out.flush();
